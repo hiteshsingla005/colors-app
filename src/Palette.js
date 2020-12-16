1d7b02f0c3cdc9a4769d4d493c024a70b42a0780
Hitesh Singla <hitesh.singla@GRAYMATTER.COM>
2020-12-16 13:31:18 +0530

    First commit with Palette component

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Palette = () => {
+    return (
+        <div className="Palette">
+            {/* TODO: Navbar */}
+            <div className="Palette-colors">
+                {/* TODO: color-box component */}
+            </div>
+            {/* TODO: Footer */}
+        </div>
+    );
+};
+
+export default Palette;
